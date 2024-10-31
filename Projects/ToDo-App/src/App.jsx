d472da2244c01f2b8f2a286cb9e7803d630f5d6e
Aaryan-Sharma-5 <aaryan2sharma5@gmail.com>
2024-10-31 12:13:38 +0530

    completed ToDo app 1.0 and added components and styling

--- Projects/ToDo-App/src/App.jsx
+++ Projects/ToDo-App/src/App.jsx
@@ -1,44 +1,18 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import "./App.css";
+
 function App() {
-  return <center class = "todo-container">
-    <h1>ToDo App</h1>
-    <div class = "container text-center">
-      <div class = "row">
-        <div class="col-6">
-          <input type="text" placeholder="Enter work" />
-        </div>
-        <div class="col-4">
-        <input type="date" />
-        </div>
-        <div class="col-2">
-        <button type="button" class="btn btn-success">Add</button>
-        </div>
-      </div>
-
-      <div class = "row">
-        <div class="col-6">
-          Complete React
-        </div>
-        <div class="col-4">
-        25 / 12 / 2024
-        </div>
-        <div class="col-2">
-        <button type="button" class="btn btn-danger">Delete</button>
-        </div>
-      </div>
-
-      <div class = "row">
-        <div class="col-6">
-          LeetCode
-        </div>
-        <div class="col-4">
-        25 / 12 / 2024
-        </div>
-        <div class="col-2">
-        <button type="button" class="btn btn-danger">Delete</button>
-        </div>
-      </div>
-    </div>
-  </center>
+  return (
+    <center class="todo-container">
+      <AppName></AppName>
+      <AddTodo></AddTodo>
+      <TodoItem1></TodoItem1>
+      <TodoItem2></TodoItem2>
+    </center>
+  );
 }
 
-export default App
+export default App;
