9c5a02a6eaeb667f41d828ce59a2c97fa4d5e324
Aaryan-Sharma-5 <aaryan2sharma5@gmail.com>
2024-10-31 17:28:16 +0530

    Learned Map Method in React

--- Fragments/src/App.jsx
+++ Fragments/src/App.jsx
@@ -3,17 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  let subjects = ["Maths", "Science", "English", "History", "Geography", "Computer Science"];
   return (
     // React.Fragment is used to wrap multiple elements without adding extra nodes to the DOM.
     <> 
       <div>
-        <h1>List Items</h1>
-        <ul class="list-group">
-          <li class="list-group-item">An item</li>
-          <li class="list-group-item">A second item</li>
-          <li class="list-group-item">A third item</li>
-          <li class="list-group-item">A fourth item</li>
-          <li class="list-group-item">And a fifth one</li>
+        <h1>Subjects</h1>
+        <ul className ="list-group">
+          {subjects.map(item =>  <li className="list-group-item">{item}</li>)}
         </ul>
       </div>
     </> 
