c2baecbcfbcc8e5a640e181db93cca74b2e5697f
Aaryan-Sharma-5 <aaryan2sharma5@gmail.com>
2024-10-31 17:39:09 +0530

    Practiced Conditional Rendering in React

--- Fragments/src/App.jsx
+++ Fragments/src/App.jsx
@@ -4,11 +4,14 @@ import "./App.css";
 
 function App() {
   let subjects = ["Maths", "Science", "English", "History", "Geography", "Computer Science"];
+  let emptyMessage = subjects.length === 0 ? <h3>No Subjects Available</h3> : null
+
   return (
     // React.Fragment is used to wrap multiple elements without adding extra nodes to the DOM.
     <> 
       <div>
         <h1>Subjects</h1>
+        {emptyMessage}
         <ul className ="list-group">
           {subjects.map(item =>  <li className="list-group-item">{item}</li>)}
         </ul>
