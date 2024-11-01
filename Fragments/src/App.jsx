1072bfc0a72cc012c23639dfdf2504a3651feabb
Aaryan-Sharma-5 <aaryan2sharma5@gmail.com>
2024-11-01 10:19:03 +0530

    Passing components as children

--- Fragments/src/App.jsx
+++ Fragments/src/App.jsx
@@ -2,6 +2,7 @@ import SubjectNames from "./components/SubjectNames";
 import ErrorMessage from "./components/ErrorMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Container from "./components/Container";
 
 function App() {
   let subjectNames = [
@@ -14,11 +15,11 @@ function App() {
 
   return (
     // React.Fragment is used to wrap multiple elements without adding extra nodes to the DOM.
-    <>
+    <Container>
         <h1 className="header">Subjects</h1>
         <ErrorMessage items={subjectNames}></ErrorMessage>
         <SubjectNames items={subjectNames}></SubjectNames>
-    </>
+    </Container>
   );
 }
 
