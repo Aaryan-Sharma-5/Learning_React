501943b0e807a14e70781afd3b930fd561ad3447
Aaryan-Sharma-5 <aaryan2sharma5@gmail.com>
2024-11-07 21:49:55 +0530

    Form from react-router-dom and implement createPostAction for form submission

--- Projects/Social-Media/src/main.jsx
+++ Projects/Social-Media/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./routes/App.jsx";
 import PostCarousel, { postLoader } from "./components/PostCarousel.jsx";
-import CreatePost from "./components/CreatePost.jsx";
+import CreatePost, { createPostAction } from "./components/CreatePost.jsx";
 
 const router = createBrowserRouter([
   {
@@ -12,11 +12,13 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <PostCarousel></PostCarousel>, loader: postLoader
+        element: <PostCarousel></PostCarousel>,
+        loader: postLoader,
       },
       {
         path: "/create-post",
         element: <CreatePost></CreatePost>,
+        action: createPostAction,
       },
     ],
   },
